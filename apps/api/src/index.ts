f26ecd2459f8bbb9263fbfab009d9747cb954f53
Harshit2786 <harshit.2002.singh@gmail.com>
2024-12-26 01:47:03 +0530

    few endpoints

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import 'dotenv/config';
 import authRouter from "./routes/login"
+import zapRouter from "./routes/zaps";
 
 const PORT=process.env.PORT;
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth',authRouter);
+app.use('/zaps',zapRouter);
 app.get('/' , (req : Request, res : Response) => {
     res.status(200).send("Hello World");
     return;
--- apps/api/src/routes/zaps.ts
+++ apps/api/src/routes/zaps.ts
@@ -7,31 +7,133 @@ import { middlewareFunc } from "../middleware";
 
 const router = express.Router();
 
-router.get('/' , middlewareFunc , async(req : Request , res : Response) => {
+const ZapPostSchema = z.object({
+    name: z.string(),
+    description: z.string(),
+    triggerId: z.string(),
+    actions: z.array(z.object({
+        order: z.number(),
+        actionsId: z.string(),
+        metaData: z.object({}).passthrough()
+    }))
+})
+
+router.get('/', middlewareFunc, async (req: Request, res: Response) => {
     const id = req.userId as string;
-    try{
+    try {
         const resp = await client.zap.findMany({
-            where : {
-                userId : id
+            where: {
+                userId: id
             },
-            include : {
-                actions : {
-                    select : {
-                        type : true,
-                        order : true
+            include: {
+                actions: {
+                    select: {
+                        type: true,
+                        order: true
                     }
                 },
-                trigger : {
-                    select : {
-                        type : true,
+                trigger: {
+                    select: {
+                        type: true,
                     }
                 }
             }
-        })
-    } catch(e){
+        });
+        res.json({ data: resp });
+        return;
+    } catch (e) {
         console.log(e);
-
+        res.status(400).json({ error: "Something went wrong" });
+        return;
     }
 });
+
+router.delete('/:zapId', middlewareFunc, async (req: Request, res: Response) => {
+    const id = req.userId as string;
+    const zapId = req.params.zapId as string;
+    try {
+        await client.zap.delete({
+            where: {
+                id: zapId,
+                userId: id
+            }
+        });
+        res.json({ message: "Zap Deleted Successfully" })
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ error: "Something went wrong" })
+    }
+})
+
+router.post('/', middlewareFunc, async (req: Request, res: Response) => {
+    const id = req.userId as string;
+    const { data, success } = ZapPostSchema.safeParse(req.body);
+    if (!success) {
+        res.status(422).json({ error: "Unprocessable data" });
+        return;
+    }
+    const timeStamp = Date.now().toString();
+    try {
+        const resp = await client.zap.create({
+            data: {
+                timeStamp,
+                name: data.name,
+                description: data.description,
+                userId: id,
+                trigger: {
+                    create: {
+                        typeId: data.triggerId
+                    }
+                },
+                actions: {
+                    createMany: {
+                        data: data.actions.map(a => ({ typeId: a.actionsId, order: a.order, metaData: a.metaData }))
+                    }
+                }
+            }
+        });
+        res.json({ data: resp });
+        return;
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ error: "Something went wrong" });
+        return;
+    }
+
+});
+
+router.get('/:zapId', middlewareFunc, async (req: Request, res: Response) => {
+    try {
+        const id = req.userId as string;
+        const zapId = req.params.zapId as string;
+        const resp = await client.zap.findUnique({
+            where: {
+                userId: id,
+                id: zapId
+            },
+            include: {
+                actions: {
+                    select: {
+                        type: true,
+                        order: true,
+                        metaData: true
+                    }
+                },
+                trigger: {
+                    select: {
+                        type: true,
+                    }
+                }
+            }
+        });
+        res.json({ data: resp });
+        return;
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ error: "Something went wrong" });
+        return;
+    }
+});
+
 
 export default router;
